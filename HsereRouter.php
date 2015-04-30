5b2e3456a19afdcf22d7bfe02961b0a3385fb40f
helloqnc <46673405@qq.com>
2015-04-30 12:50:10 +0800

    首次调用

--- HsereRouter.php
+++ HsereRouter.php
@@ -6,9 +6,13 @@ namespace HsereTech\HsereRouter;
 
  */
 
-echo 'loaded';
-class HsereRouter
-{
-
+class HsereRouter {
+    function __construct ()
+    {
+        echo 'function loaded';
+    }
+    static function router() {
+        echo 'router loaded';
+    }
 
 }
